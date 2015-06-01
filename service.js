57df30734c8f4469c7ff2a1371870d679c3e0756
rochal <piotr.rochala@gmail.com>
2015-06-02 09:38:11 +1000

    push new service file

--- service.js
+++ service.js
@@ -1,24 +1,17 @@
 
 var amqp = require('amqplib');
-var elasticsearch = require('elasticsearch');
 var _ = require('lodash');
 
-var client = new elasticsearch.Client({
-  host: 'idctjpocapp001.tj.svc.sb.int:9200',
-  log: 'trace'
-});
-
-var queue = [];
 var queue_size = 100;
 
-amqp.connect('amqp://piotrr:sports99@idctjpocapp001.tj.svc.sb.int:5672/myvhost').then(function(conn) {
+amqp.connect('amqp://porky:porky@192.168.0.52:15672/porky').then(function(conn) {
   process.once('SIGINT', function() { conn.close(); });
   return conn.createChannel().then(function(ch) {
 
-    var ok = ch.assertQueue('myqueue');
+    var ok = ch.assertQueue('');
 
     ok = ok.then(function(myqueue) {
-      return ch.consume('myqueue', function(msg) {
+      return ch.consume('', function(msg) {
         console.log(" [x] Received '%s'", msg.content.toString());
 
         var parsed = JSON.parse(msg.content.toString());
@@ -42,47 +35,3 @@ amqp.connect('amqp://piotrr:sports99@idctjpocapp001.tj.svc.sb.int:5672/myvhost')
     });
   });
 }).then(null, console.warn);
-
-// pushToES([
-
-// { ev_oc_id: 167022400,
-//   status: 'S',
-//   displayed: 'Y',
-//   result: 'L',
-//   result_conf: 'Y',
-//   lp_num: 40000,
-//   lp_den: 1000,
-//   settled: 'Y',
-//   desc: '|Half Time Score| |Cd Suchitepequez| 2-0 - |Full Time Score| |Cd Suchitepequez| 2-0' },
-// { ev_oc_id: 167022401,
-//   status: 'S',
-//   displayed: 'Y',
-//   result: 'L',
-//   result_conf: 'Y',
-//   lp_num: 500000,
-//   lp_den: 1000,
-//   settled: 'Y',
-//   desc: '|Half Time Score| |CSD Municipal| 2-0 - |Full Time Score| |CSD Municipal| 5-1' }
-
-// ]);
-
-function pushToES(data) {
-
-  console.log('pushing to ES', data);
-
-  var request = [];
-  _.map(data, function(item){
-    request.push({ update: { _index: 'all_outcomes', _type: 'all_outcomes', _id: item.ev_oc_id } });
-    request.push({ doc: item, "doc_as_upsert" : true });
-  });
-
-  client.bulk({
-    body: request
-  }, function (err, resp) {
-    if (!err) {
-      console.log('Updated 100 rows...');
-    } else {
-      console.warn(err);
-    }
-  });
-}
