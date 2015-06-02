91387396a1a59b177cb757bcf4d40a18acb38bb1
Sebastien Peek <sebastein.peek@gmail.com>
2015-06-02 12:35:03 +1000

    Added in unique id to NeedPacket

--- node/needPacket.js
+++ node/needPacket.js
@@ -3,10 +3,13 @@ module.exports = function (msg) {
 	var NEED = "car_rental_offer";
 	var solutions = [];
 
+	var uuid = require('uuid').v4();
+
 	var message = msg || {
 		"json_class" : "NeedPacket",
 		"need": NEED,
-		"solutions": solutions
+		"solutions": solutions,
+		"_id" : uuid
 	};
 
 	this.stringify = function () {
